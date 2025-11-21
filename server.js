c32091d197b95fdd658aeba33a69a52d8a023a59
DanielAntonioSalcidoGutierrez19 <142370044+DanielAntonioSalcidoGutierrez19@users.noreply.github.com>
2025-11-21 16:15:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -56,9 +56,7 @@ app.post("/send-activity-email", async (req, res) => {
   }
 });
 
-// SOLO UNA VEZ
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor EducaKids escuchando en puerto", PORT);
 });
-
